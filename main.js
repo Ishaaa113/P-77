1345c673ac087b7a581f854aa3abefc989032f7f
Ishaaa113 <94088187+Ishaaa113@users.noreply.github.com>
2022-01-19 19:42:53 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var first_name = "Ishaan" 
+var last_name = "abilash"
+var a = 10;
+var b = 5;
+var add = a +b;
+console.log(add);
+console.log(first_name.concat(last_name))
+var name_one = Hi, i am Ishaan Abilash
+console.log(name_one);
